6ff4d8e95d2a5450254d3dea846be1a6a4e02dc3
Nikhil Adyapak <56066146+NikhilAdyapak@users.noreply.github.com>
2026-06-29 20:49:33 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # for i, s in enumerate(strs):
+        #     strs[i] = Counter(s)
+
+        def count_gen(s):
+            arr = [0] * 26
+            for i, char in enumerate(s):
+                arr[ord(char) - ord('a')] += 1
+            return tuple(arr)
+
+        # seen = {}
+        seen = defaultdict(list)
+        # print(seen, type(seen))
+        for s in strs:
+            dict_key = count_gen(s)
+            if dict_key in seen:
+                seen[dict_key].append(s)
+            else:
+                seen[dict_key] = [s]
+        return list(seen.values())
